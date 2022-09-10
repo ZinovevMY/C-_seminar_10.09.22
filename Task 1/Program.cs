bd1d0a36d96f3e3e465dea3aa85efcd44a465c94
Mikhail <mehan@list.ru>
2022-09-10 15:06:00 +0300

    Done

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Prompt("Введите число: ");
+
+int NumCount(int num)
+{
+    int counter = 0;
+    while (num > 0)
+    {
+        num = num/10;
+        counter++;
+    }
+    return counter;
+
+}
+int numcount = NumCount(number);
+System.Console.WriteLine($"В числе {number} {numcount} цифр.");

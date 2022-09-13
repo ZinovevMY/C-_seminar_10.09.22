bb544a7c3c4af2f2e9c5e11aaa59e5155bb3863b
Mikhail <mehan@list.ru>
2022-09-13 20:32:39 +0300

    Done

--- Homework/Task 2/Program.cs
+++ Homework/Task 2/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+const int MIN_ELEMENTS = -10;
+const int MAX_ELEMENTS = 50;
+const int ARRAY_SIZE   = 8;
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] array = GenerateArray(ARRAY_SIZE, MIN_ELEMENTS, MAX_ELEMENTS);
+PrintArray(array);

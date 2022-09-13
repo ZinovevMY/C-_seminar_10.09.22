4dc108a060facb029c2765fba8a42e918f527814
Mikhail <mehan@list.ru>
2022-09-13 20:25:27 +0300

    Done

--- Homework/Task 1/Program.cs
+++ Homework/Task 1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumOfNumbers(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num%10;
+        num = num/10;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число: ");
+System.Console.WriteLine($"Сумма чисел, составляющих число {number} равна {SumOfNumbers(number)}");
